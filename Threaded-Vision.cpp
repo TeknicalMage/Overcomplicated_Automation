7a9f404f036844a9ff2d171d7384d3457d025fbf
Teknical Mage <arcazjordan@gmail.com>
2021-11-12 07:22:42 -0500

    Added kill features, and speed up ttk for threads

--- Threaded-Vision.cpp
+++ Threaded-Vision.cpp
@@ -19,7 +19,12 @@ using namespace std;
 
 int hits = 0;
 
+//thread mgmt section
 
+static bool proc_lock = true;
+
+
+//////////////////////
     //C Keypress
 void C_Key_input(){
     SHORT key;
@@ -55,32 +60,6 @@ void F3_Key_input(){
     //F3 Keypress
 };
 
-    //Move Forward Input
-void fuckaboutforward(){
-    std::cout << "Move Forward \n\n";
-    SHORT key;
-    UINT mappedkey;
-    INPUT input = { 0 };
-    key = VkKeyScan('W'); 
-    mappedkey = MapVirtualKey(LOBYTE(key), 0);
-    input.type = INPUT_KEYBOARD;
-    input.ki.dwFlags = KEYEVENTF_SCANCODE;
-    input.ki.wScan = mappedkey;
-    SendInput(1, &input, sizeof(input));
-    //Keydown time determines Distance
-    //std::this_thread::sleep_for(std::chrono::milliseconds(339213));
-
-    //std::this_thread::sleep_for(std::chrono::milliseconds(11940)); | 51.587
-    //std::this_thread::sleep_for(std::chrono::milliseconds(11540)); | 49.86
-    //std::this_threC:\Users\arcaz\Documents\Programming Projects\Self_Loathing\Overcomplicated_Automationad::sleep_for(std::chrono::milliseconds(11640)); | 50.291
-    //std::this_thread::sleep_for(std::chrono::milliseconds(11600)); | 50.076 per input
-    std::this_thread::sleep_for(std::chrono::milliseconds(11600000));
-    input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
-    SendInput(1, &input, sizeof(input));
-};
-    //Move Forward Input        
-
-
 string Coord_Rip (){
 	OpenClipboard(0);
 	const char* op = "Where in tf am I";
@@ -104,14 +83,58 @@ string Coord_Rip (){
 };
 
 void loc_detect(){
-    while(true){
+    proc_lock = true;
+    while(proc_lock){
         //F3 + C -- The Keyboard shortcut to show coords
         F3_Key_input();
         cout << Coord_Rip();
-        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
         //Side notee. Sleep is in Mili seconds
     }
 }
+
+//////////////////////
+
+
+    //Move Forward Input
+void forwardmov(){
+
+    //using namespace std::literals::chrono_literals;
+
+    std::cout << "Move Forward \n\n";
+    SHORT key;
+    UINT mappedkey;
+    INPUT input = { 0 };
+    key = VkKeyScan('W'); 
+    mappedkey = MapVirtualKey(LOBYTE(key), 0);
+    input.type = INPUT_KEYBOARD;
+    input.ki.dwFlags = KEYEVENTF_SCANCODE;
+    input.ki.wScan = mappedkey;
+    SendInput(1, &input, sizeof(input));
+    //Keydown time determines Distance
+    //std::this_thread::sleep_for(std::chrono::milliseconds(339213));
+
+    //std::this_thread::sleep_for(std::chrono::milliseconds(11940)); | 51.587
+    //std::this_thread::sleep_for(std::chrono::milliseconds(11540)); | 49.86
+    //std::this_threC:\Users\arcaz\Documents\Programming Projects\Self_Loathing\Overcomplicated_Automationad::sleep_for(std::chrono::milliseconds(11640)); | 50.291
+    //std::this_thread::sleep_for(std::chrono::milliseconds(11600)); | 50.076 per input
+    std::this_thread::sleep_for(std::chrono::milliseconds(11600000));
+    input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+    SendInput(1, &input, sizeof(input));
+
+
+    //std::this_thread::sleep_for(1s);
+};
+    //Move Forward Input        
+
+
+//Stops Coords from looping. Will use this at the emergency kill or built in ender or threads
+void merk(){
+    std::cout << "Attempted kill \n\n";
+    proc_lock = false;
+}
+
+
 
 int look(){
     {
@@ -129,7 +152,7 @@ void tick(){
             hits++;
         }
         if (GetAsyncKeyState(VK_NUMPAD2)) { 
-            std::thread forwardm(fuckaboutforward);
+            std::thread forwardm(forwardmov);
             forwardm.detach();
             std::cout << hits;
             hits++;
@@ -142,6 +165,11 @@ void tick(){
         if (GetAsyncKeyState(VK_NUMPAD3)) { 
             std::thread extra(look);
             extra.detach();
+            std::this_thread::sleep_for(std::chrono::milliseconds(20)); 
+        }
+        if (GetAsyncKeyState(VK_NUMPAD9)) { 
+            std::thread killall(merk);
+            killall.detach();
             std::this_thread::sleep_for(std::chrono::milliseconds(20)); 
         }
 
